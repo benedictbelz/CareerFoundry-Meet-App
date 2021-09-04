c5f08d8b414efc2bb3f5f072613cc54804fe3403
Benedict Belz <info@benedictbelz.eu>
2021-09-04 22:46:43 +0200

    4.6: Continuous Delivery

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as atatus from 'atatus-spa';
 import './css/index.css';
+
+atatus.config('a5d16a9e594a4a10a51316e9d18fce4f').install();
 
 ReactDOM.render(
 	<React.StrictMode>
